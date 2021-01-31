44fed0874abd1bc2e0df0954e185495b7e95bffd
nix <ngencokamin@gmail.com>
2021-01-31 12:47:29 -0800

    Adds has_many through associations for movie genres

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,9 +1,17 @@
 class Movie < ApplicationRecord
   has_many :actors
+  has_many :movie_genres
+  has_many :genres, through: :movie_genres
 
   validates :title, presence: true
   validates :director, length: { minimum: 5}
   validates :year, numericality: { greater_than: 4 }
   validates :english, inclusion: [ true, false ]
   validates :plot, length: { in: 10..500 }
+
+  def genre_names
+    genres.map do |genre|
+      genre.name
+    end
+  end
 end
